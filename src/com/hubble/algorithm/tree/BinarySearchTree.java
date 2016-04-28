6f920c57678c991f10f48b0553c13b2904779570
edjies <panhaobo1991@163.com>
2016-04-28 17:21:15 +0800

    first commit of MyAlgorithm

---
+++ src/com/hubble/algorithm/tree/BinarySearchTree.java
@@ -0,0 +1,73 @@
+package com.hubble.algorithm.tree;
+
+public class BinarySearchTree<T extends Comparable<T>> extends BinaryTreeNode<T> {
+	
+	public BinarySearchTree(T data) {
+		super(data);
+		
+	}
+	
+	/**
+	 * 增加一个节点。 O(Lg(Height))
+	 * @param newNode
+	 * @return
+	 */
+	public boolean add(BinarySearchTree<T> newNode) {
+		// parent == null 
+		if(mParent == null) {
+			mParent = newNode;
+			mSize++;
+			return true;
+		}
+		
+		BinaryTreeNode<T> node = mParent;
+		while(node != null) {
+			if(newNode.mData.compareTo(node.mData) <= 0) {
+				if(node.mLeft == null) {
+					node.mLeft = newNode;
+					newNode.mParent = node;
+					mSize++;
+					return true;
+				}
+				node = node.mLeft;
+			}else {
+				if(node.mRight == null) {
+					node.mRight = newNode;
+					newNode.mParent = node;
+					mSize++;
+					return true;
+				}
+				node = node.mRight;
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * 搜索节点 （O(Lg(Height))）
+	 * @param node
+	 */
+	public BinarySearchTree<T> search(T t) {
+		BinaryTreeNode<T> node = mParent;
+		while(node != null) {
+			if(node.mData.compareTo(t) == 0) {
+				return (BinarySearchTree<T>)node;
+			}
+			
+			else if(node.mData.compareTo(t) < 0) {
+				node = node.mLeft;
+			}
+			
+			else if(node.mData.compareTo(t) > 0) {
+				node = node.mRight;
+			}
+		}
+		
+		return null;
+	}
+	
+	
+	
+
+}
---
+++ src/com/hubble/algorithm/tree/app/ErgodicTree.java
@@ -0,0 +1,7 @@
+package com.hubble.algorithm.tree.app;
+
+public class ErgodicTree {
+	public static void main(String[] args) {
+		
+	}
+}
