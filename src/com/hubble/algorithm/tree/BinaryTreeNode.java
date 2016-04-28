c07be3d6828212f565d76759f1f30992a9f678f6
edjies <panhaobo1991@163.com>
2016-04-28 21:07:30 +0800

    重建java项目

---
+++ src/com/hubble/algorithm/tree/BinaryTreeNode.java
@@ -0,0 +1,25 @@
+package com.hubble.algorithm.tree;
+
+public class BinaryTreeNode<T> {
+	protected T mData = null;
+	protected BinaryTreeNode<T> mParent = null;
+	protected BinaryTreeNode<T> mLeft = null;
+	protected BinaryTreeNode<T> mRight = null;
+	protected int mSize = 0;
+	
+	public BinaryTreeNode(T data) {
+		this.mData = data;
+	}
+	
+	public void insert(T data) {
+		
+	}
+	
+	public void find() {
+		
+	}
+	
+	public void delete() {
+		
+	}
+}
